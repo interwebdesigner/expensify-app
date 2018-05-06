90cb09a87ba17aa5cc93e7a7cea890465bd667d8
Chris <info@interwebdesigner.co.uk>
2018-05-06 20:55:13 +0100

    Add expensify styles

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -13,7 +13,7 @@ import {
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
-const uid = 'thisismytesteuid';
+const uid = 'thisismytestuid';
 const defaultAuthState = {auth: {uid}};
 const createMockStore = configureMockStore([thunk]);
 
